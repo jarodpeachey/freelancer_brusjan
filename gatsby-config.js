d039e381fccf1beee5bb1253a752ceff854dc29b
Jarod Peachey <jwpeachey107@aol.com>
2019-10-28 20:28:28 -0400

    More updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,8 +18,8 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'brusjan-website',
+        short_name: 'brusjan',
         start_url: '/',
         background_color: '#663399',
         theme_color: '#663399',
