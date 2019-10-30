04f34dfab35f6e67d67fe9d49d885cbf7d4f7eca
Jarod Peachey <jwpeachey107@aol.com>
2019-10-30 14:17:07 -0400

    Update image

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,7 @@ module.exports = {
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/main_logo_transparent.png', // This path is relative to the root of the site.
+        icon: 'src/images/brusjan_emblem.png', // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-linaria',
